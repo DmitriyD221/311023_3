9f4197624bef0cf12e84129ce70adc0a0f633556
DmitriyD221 <143995912+DmitriyD221@users.noreply.github.com>
2023-10-31 17:39:16 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -13,7 +13,7 @@ public:
     Student* students;
     int size;
     Group() : students(nullptr), size(0) {}
-    Group(const Group& other) : size(other.size) {
+    group( const Group& other) : size(other.size) {
         students = new Student[size];
         for (int i = 0; i < size; i++) {
             students[i] = other.students[i];
@@ -26,19 +26,19 @@ public:
 
 int main() {
     Student student1("Mykhailo", 20);
-    Student student2("Mykola", 21);
+    Student  student2("Mykola", 21);
     Group group1;
     group1.students = new Student[2];
      group1.students[0] = student1;
     group1.students[1] = student2;
     group1.size = 2;
-    Group group2 = group1;
+    Group grop2 = group1;
     cout << "Інформація про групу 1::" << endl;
     for (int i = 0; i < group1.size; i++) {
-        cout << "Ñòóäåíò " << i + 1 << ": " << group1.students[i].name << ", " << group1.students[i].age << " ðîê³â" << endl;
+        cout << "Студент " << i + 1 << ": " << group1.students[i].name << ", " << group1.students[i].age << "рокыв" << endl;
     }
 
-    cout << "²íôîðìàö³ÿ ïðî Ãðóïó 2 (êîï³ÿ Ãðóïè 1):" << endl;
+    cout << "Інформація про Групу 2 (копія Групи 1)::" << endl;
     for (int i = 0; i < group2.size; i++) {
         cout << "Студент" << i + 1 << ": " << group2.students[i].name << ", " << group2.students[i].age << "рокыв << endl;
     }
